61c7a76f6dc352ada4c33db2cd4c9378856ede44
AfinMuhammed <afin.ta@techversantinfo.com>
2022-04-20 09:20:22 +0530

    update

--- accordion/js/app.js
+++ accordion/js/app.js
@@ -4,13 +4,13 @@
       // alert(this);
       if($(this).hasClass("active")){
         $(this).removeClass("active").next().stop(true).slideUp();
-        $(this).children("i").removeClass("fa-solid fa-circle-chevron-up white").addClass("fa-solid fa-circle-chevron-down icon");
+        $(this).children("i").removeClass("fa-circle-chevron-up white").addClass("fa-circle-chevron-down icon");
       }
       else{
         $(".contentHead").removeClass("active").next().stop(true).slideUp();
-        $(".contentHead").children("i").removeClass("fa-solid fa-circle-chevron-up white").addClass("fa-solid fa-circle-chevron-down icon");
+        $(".contentHead").children("i").removeClass("fa-circle-chevron-up white").addClass("fa-circle-chevron-down icon");
         $(this).addClass("active").next().stop(true).slideDown(); 
-        $(this).children("i").removeClass("fa-solid fa-circle-chevron-down icon").addClass("fa-solid fa-circle-chevron-up white");
+        $(this).children("i").removeClass("fa-circle-chevron-down icon").addClass("fa-circle-chevron-up white");
         
       }
     })
